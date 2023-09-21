8ee2e21e8fc82b89d80016a04fb1c8567b4f52b9
Максим <wintry.wisper@mail.ru>
2023-09-21 18:47:52 +0300

    Выполнено: сделано подчеркивание для заголовка,форма поиска тура, JS скрипт для autoComplete(есть в  index.js в комментариях),блок подсказки

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,34 @@
-import mobileNav from './modules/mobile-nav.js'
-mobileNav();
+// В слудеющий раз
+//import autoComplete from "@tarekraafat/autocomplete.js";
+//const countryList[]
+//
+// const autoCompleteJS = new autoComplete({
+//   selector: "#autoCompleteCountry",
+//   placeHolder: "e.g Bali, Indonesia",
+//   data: {
+//     src: [
+//       "Sauce - Thousand Island",
+//       "Wild Boar - Tenderloin",
+//       "Goat - Whole Cut",
+//     ],
+//   },
+//   resultItem: {
+//     highlight: true,
+//   },
+//     events: {
+//         input: {
+//             selection: (event) => {
+//                 const selection = event.detail.selection.value;
+//                 autoCompleteJS.input.value = selection;
+//             },
+//         },
+//     },
+//     resultsList {
+//       maxResults:10;
+//     }
+//
+// });
+
+// Мобильная навигация!!!
+//import mobileNav from './modules/mobile-nav.js'
+//mobileNav();
